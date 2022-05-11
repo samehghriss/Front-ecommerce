9c26bd37a66b0c211776301781bcd215e7c24084
samehghriss <ghriiss12@gmail.com>
2022-05-11 01:21:11 +0100

    first commit

---
+++ src/core/FixedPrice.js
@@ -0,0 +1,32 @@
+export const prices = [
+  {
+      _id: 0,
+      name: "Any",
+      array: [] //going to send to the backend
+  },
+//   {
+//       _id: 1,
+//       name: "Under ₹99",
+//       array: [0, 999]
+//   },
+  {
+      _id: 2,
+      name: "Dt100 to Dt199",
+      array: [100, 199]
+  },
+  {
+      _id: 3,
+      name: "Dt200 to Dt299",
+      array: [200, 299]
+  },
+  {
+      _id: 4,
+      name: "Dt300 to Dt399",
+      array: [300, 399]
+  },
+  {
+      _id: 5,
+      name: "Over Dt400",
+      array: [400, 650]
+  }
+];
